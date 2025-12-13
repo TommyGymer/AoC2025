c37c50a13d58de63e9b909ea7123b14511e7e411
Tom Gymer <tommygymer@gmail.com>
2025-12-13 12:53:45 +0000

    part 1

--- d10/src/main.rs
+++ d10/src/main.rs
@@ -1,3 +1,120 @@
+use std::collections::VecDeque;
+
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+
+#[derive(Debug)]
+struct Machine {
+    lights: Vec<bool>,
+    buttons: Vec<Vec<usize>>,
+    joltages: Vec<usize>,
+}
+
+impl From<&str> for Machine {
+    fn from(value: &str) -> Self {
+        let mut parts = value.split(' ');
+        Self {
+            lights: parts
+                .next()
+                .unwrap()
+                .replace("[", "")
+                .replace("]", "")
+                .chars()
+                .map(|c| match c {
+                    '.' => false,
+                    '#' => true,
+                    _ => panic!(),
+                })
+                .collect(),
+            buttons: parts
+                .to_owned()
+                .take_while(|p| p.starts_with('('))
+                .map(|button| {
+                    button
+                        .replace("(", "")
+                        .replace(")", "")
+                        .split(',')
+                        .map(|i| usize::from_str_radix(i, 10).unwrap())
+                        .collect()
+                })
+                .collect(),
+            joltages: parts
+                .last()
+                .unwrap()
+                .replace("{", "")
+                .replace("}", "")
+                .split(',')
+                .map(|i| usize::from_str_radix(i, 10).unwrap())
+                .collect(),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Queued {
+    depth: usize,
+    lights: Vec<bool>,
+}
+
+fn fewest_buttons(lights: Vec<bool>, buttons: Vec<Vec<usize>>) -> usize {
+    let mut queue: VecDeque<Queued> = VecDeque::new();
+    queue.push_back(Queued {
+        depth: 0,
+        lights: lights,
+    });
+
+    loop {
+        let item = queue.pop_front().unwrap();
+        // NOTE: check if all the lights are off:
+        // by going from the required state to the starting all off state
+        if item.lights.iter().all(|l| !*l) {
+            return item.depth;
+        } else {
+            let _ = buttons
+                .iter()
+                .map(|button| {
+                    let mut lights = item.lights.to_owned();
+                    let _ = button
+                        .iter()
+                        .map(|i| *lights.get_mut(*i).unwrap() = !lights.get(*i).unwrap())
+                        .collect::<()>();
+                    queue.push_back(Queued {
+                        depth: item.depth + 1,
+                        lights,
+                    });
+                })
+                .collect::<()>();
+        }
+    }
+}
+
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
+
+    let machine_line = input.split('\n');
+    let machines: Vec<Machine> = machine_line
+        .filter(|line| line.len() > 0)
+        .map(|line| Machine::from(line))
+        .collect();
+
+    let res: usize = machines
+        .into_par_iter()
+        .map(|machine| fewest_buttons(machine.lights, machine.buttons))
+        .sum();
+
+    println!("{}", res);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fewest_buttons() {
+        let res = fewest_buttons(
+            vec![false, true, true, true, false, true],
+            vec![vec![0, 3, 4], vec![0, 1, 2, 4, 5]],
+        );
+
+        assert_eq!(res, 2);
+    }
 }
