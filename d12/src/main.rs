da1e04bc5cec7a57ba41d10516f752c44f6d9b98
Tom Gymer <tommygymer@gmail.com>
2025-12-17 22:43:26 +0000

    a bit

--- d12/src/main.rs
+++ d12/src/main.rs
@@ -1,5 +1,12 @@
+#[derive(Debug, Clone, Copy)]
 struct Shape {
     shape: [[bool; 3]; 3],
+}
+
+#[derive(Debug)]
+struct Area {
+    size: (u32, u32),
+    shapes: Vec<u8>,
 }
 
 impl From<&Vec<&str>> for Shape {
@@ -29,4 +36,52 @@ impl From<&Vec<&str>> for Shape {
 
 fn main() {
     let input = std::fs::read_to_string("example.txt").unwrap();
+
+    let sections = input.split("\n\n");
+    // NOTE: both inputs have 6 shapes
+    let mut shapes: Vec<Shape> = Vec::with_capacity(6);
+    let mut areas: Vec<Area> = Vec::new();
+
+    let _ = sections
+        .into_iter()
+        .filter(|s| s.len() > 0)
+        .map(|section| {
+            let parts = section.split('\n');
+            if section.contains('x') {
+                // handle area
+                areas = parts
+                    .filter(|l| l.len() > 0)
+                    .map(|line| {
+                        let mut bits = line.split(": ");
+                        let mut xy = bits.next().unwrap().split('x');
+                        let (x, y) = (
+                            u32::from_str_radix(xy.next().unwrap(), 10).unwrap(),
+                            u32::from_str_radix(xy.next().unwrap(), 10).unwrap(),
+                        );
+                        let shapes = bits
+                            .next()
+                            .unwrap()
+                            .split(' ')
+                            .map(|s| u8::from_str_radix(s, 10).unwrap())
+                            .collect();
+
+                        Area {
+                            shapes,
+                            size: (x, y),
+                        }
+                    })
+                    .collect();
+            } else {
+                // shapes are discovered in order, so we can ignore the index line
+                shapes.push(Shape::from(&parts.skip(1).take(3).collect::<Vec<&str>>()));
+            }
+        })
+        .collect::<()>();
+
+    println!("{:?}", shapes);
+    println!("{:?}", areas);
+
+    let res: u32 = areas.into_iter().map(|area| {}).sum();
+
+    println!("{}", res)
 }
