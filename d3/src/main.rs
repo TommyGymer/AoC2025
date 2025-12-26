5745535b6f831b592946cfda335b83715f8aa286
Tom Gymer <tommygymer@gmail.com>
2025-12-26 17:20:33 +0000

    impl bucket day 3

--- d3/src/main.rs
+++ d3/src/main.rs
@@ -1,4 +1,4 @@
-use std::{array, fs};
+use std::{array, collections::VecDeque, fs};
 
 fn insert_select_n_from_k(battery: &Vec<u8>, n: usize) -> u64 {
     (0..n)
@@ -21,15 +21,56 @@ fn insert_select_n_from_k(battery: &Vec<u8>, n: usize) -> u64 {
 
 fn bucket_select_n_from_k(battery: &Vec<u8>, n: usize) -> u64 {
     // sort
-    let mut buckets: [Vec<usize>; 9] = array::from_fn(|_| Vec::with_capacity(battery.len()));
-    let _ = battery
-        .iter()
-        .enumerate()
-        .map(|(i, b)| buckets[*b as usize].push(i))
+    let mut buckets: [VecDeque<(usize, u8)>; 9] =
+    // NOTE: there is no noticable performance decrease with this capacity
+        array::from_fn(|_| VecDeque::with_capacity(battery.len() / 9));
+    let mut battery_iter = battery.into_iter().enumerate();
+    let _ = battery_iter
+        .by_ref()
+        .take(battery.len() - n)
+        .map(|(i, b)| buckets[*b as usize - 1].push_back((i, *b)))
         .collect::<()>();
 
     // select
-    0
+    (0..n)
+        .fold((0usize, 0u64), |acc, _| {
+            // push next battery into buckets to maintain buffer at end
+            let next_battery = battery_iter.next().unwrap();
+            buckets[*next_battery.1 as usize - 1].push_back((next_battery.0, *next_battery.1));
+
+            let mut selected_battery = None;
+
+            while selected_battery.is_none() {
+                let next = if let Some(item) = buckets[8].pop_front() {
+                    item
+                } else if let Some(item) = buckets[7].pop_front() {
+                    item
+                } else if let Some(item) = buckets[6].pop_front() {
+                    item
+                } else if let Some(item) = buckets[5].pop_front() {
+                    item
+                } else if let Some(item) = buckets[4].pop_front() {
+                    item
+                } else if let Some(item) = buckets[3].pop_front() {
+                    item
+                } else if let Some(item) = buckets[2].pop_front() {
+                    item
+                } else if let Some(item) = buckets[1].pop_front() {
+                    item
+                } else if let Some(item) = buckets[0].pop_front() {
+                    item
+                } else {
+                    unreachable!("someone did a silly");
+                };
+                if next.0 >= acc.0 {
+                    selected_battery = Some(next);
+                }
+            }
+            let selected_battery = selected_battery.expect("someone did a silly");
+
+            (selected_battery.0, acc.1 * 10 + selected_battery.1 as u64)
+        })
+        .1
 }
 
 fn main() {
@@ -45,7 +86,7 @@ fn main() {
                 .collect::<Vec<u8>>()
         })
         .filter(|l| l.len() > 0)
-        .map(|battery| insert_select_n_from_k(&battery, 2))
+        .map(|battery| bucket_select_n_from_k(&battery, 12))
         .sum();
 
     println!("{}", total)
