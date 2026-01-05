19e2698a1fc90d5b2471ba70e44797d657684c3b
Tom Gymer <tommygymer@gmail.com>
2026-01-05 11:03:54 +0000

    d1

--- y2015/d1/src/main.rs
+++ y2015/d1/src/main.rs
@@ -1,3 +1,25 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").unwrap();
+
+    // part 1
+    let ups = input.chars().filter(|c| *c == '(').count();
+    let downs = input.chars().filter(|c| *c == ')').count();
+
+    println!("{}", ups - downs);
+
+    // part 2
+    let mut acc = 0i64;
+    for (i, c) in input.char_indices() {
+        acc = match c {
+            '(' => acc + 1,
+            ')' => acc - 1,
+            _ => acc,
+        };
+        if acc < 0 {
+            println!("{}", i + 1);
+            break;
+        };
+    }
 }
